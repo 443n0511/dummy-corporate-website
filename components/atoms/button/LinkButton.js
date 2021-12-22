d26a8318dd151e2e9efa7c0393b504e502bdc583
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2021-12-22 23:14:14 +0900

    Fix Fixing tags

--- components/atoms/button/LinkButton.js
+++ components/atoms/button/LinkButton.js
@@ -5,18 +5,20 @@ import NextLink from "next/link";
 
 export const LinkButton = memo(({ href, children, margin }) => {
   return (
-    <Button
-      as='p'
-      p={8}
-      m={margin}
-      borderRadius='full'
-      variant='outline'
-      colorScheme='gray.600'
-      size={{ base: "md", lg: "lg" }}
-      _hover={{ cursor: "pointer", bgColor: "gray.600", color: "white" }}
-      rightIcon={<ArrowForwardIcon />}
-      fontSize={{ base: "md", lg: "lg" }}>
-      <NextLink href={href}>{children}</NextLink>
-    </Button>
+    <NextLink href={href} passHref>
+      <Button
+        as='a'
+        p={8}
+        m={margin}
+        borderRadius='full'
+        variant='outline'
+        colorScheme='gray.600'
+        size={{ base: "md", lg: "lg" }}
+        _hover={{ cursor: "pointer", bgColor: "gray.600", color: "white" }}
+        rightIcon={<ArrowForwardIcon />}
+        fontSize={{ base: "md", lg: "lg" }}>
+        {children}
+      </Button>
+    </NextLink>
   );
 });
