d0af7971c9b18642ab28ec789f8a6d6f509e7a9a
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2022-01-14 21:18:10 +0900

    refactor Fixing the title

--- pages/news/page/[id].js
+++ pages/news/page/[id].js
@@ -9,7 +9,7 @@ export default function NewsPageId({ news, totalCount }) {
   console.log(news);
   return (
     <Layout
-      title='株式会社Sample'
+      title='お知らせ一覧 | 株式会社Sample'
       description='株式会社Sampleのお知らせ一覧ページ'>
       <Box
         as='section'
