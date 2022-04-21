b58d7010371145ffe2ac97d1696831374beebff3
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2022-04-21 20:27:18 +0900

    fix: client

--- api/client.js
+++ api/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "microcms-js-sdk";
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
+  serviceDomain: "monaca",
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
 });
