ce79f9c64c1390d15429039a0fc6723aed5a2137
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2022-04-21 23:08:54 +0900

    fix: Environment Variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const { NEXT_PUBLIC_API_KEY, NEXT_PUBLIC_SERVICE_DOMAIN } = process.env;
+
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_SERVICE_DOMAIN,
+  },
+};
