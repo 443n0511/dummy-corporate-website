9c481508fcd8e5d9c8109ee095ea2c3b77643ee5
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2022-04-21 18:56:34 +0900

    fix: Correction of data acquisition process

--- api/client.js
+++ api/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "microcms-js-sdk";
 export const client = createClient({
-  serviceDomain: NEXT_PUBLIC_SERVICE_DOMAIN,
-  apiKey: NEXT_PUBLIC_API_KEY,
+  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
 });
